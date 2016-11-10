91fcaa3a4fcdbcc06afae14a40ec72a2026ed2bd
rsantos <rsantos@braziliandevs.com>
2016-11-10 20:19:20 -0200

    Added global dependencies

--- deploy.cmd
+++ deploy.cmd
@@ -100,6 +100,10 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
+  :: Install required global dependencies to build
+  call :ExecuteCmd !NPM_CMD! install webpack typescript
+
+  :: Install package dependencies
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
